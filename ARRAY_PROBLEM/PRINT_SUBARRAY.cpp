1d9e564c06f47cdbdc0cd34c3d4a8f42f0690248
AyushMBhalerao <ayushmbha98@gmail.com>
2025-08-21 00:48:56 +0530

    progress

---
+++ ARRAY_PROBLEM/PRINT_SUBARRAY.cpp
@@ -0,0 +1,26 @@
+// write a program to print all possible subarray 
+#include <iostream>
+using namespace std;
+
+int main (){
+
+    int arr [] = {1,2,3,4,5};
+    int n = sizeof(arr)/ sizeof(int);
+
+    for (int i=0;i<n;i++){
+
+        for(int j=i;j<n;j++){
+
+            for(int k=i;k<=j;k++){
+
+                cout << arr[k];
+
+            }
+            cout << ",";
+
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
