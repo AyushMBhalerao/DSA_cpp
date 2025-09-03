7f6857223a682bcc1eec7c7364a3dacb801c17f6
AyushMBhalerao <ayushmbha98@gmail.com>
2025-09-03 14:45:58 +0530

    Fixed bubble sort implementation

--- BASIC_SORTING /bubble_sort.cpp
+++ BASIC_SORTING /bubble_sort.cpp
@@ -7,11 +7,16 @@ int main (){
     int n = sizeof(arr)/sizeof(int);
     
     for (int i=0;i<n-1;i++){
+        bool is_swap = false ;
         
         for(int j=0;j<n-i-1;j++){
             if (arr[j]>arr[j+1]){
                 swap (arr[j+1],arr[j]);
+                is_swap = true ;
             }
+        }
+        if ( is_swap == false){
+            break ; 
         }
     }
     for (int i =0;i<n;i++){
